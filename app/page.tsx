60f76101f9e1d1059a3bebad20a927e4e5c3504c
felipe.costa <felipe.costa@synvia.com>
2023-07-06 20:19:10 -0300

    feat: add files netx project and more first landipage construction

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,81 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import synviaImage from '../image/synvia.png';
+import logoImage from '../image/logo.png';
+import footergreenImage from '../image/footer-green.png';
+import './page.css';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="flex justify-center items-center h-screen">
+        <div className="relative">
+          <Image
+            src={logoImage}
+            alt="Logo"
+            className="ml-1"
+            width={300}
+            // height={300}
+          />
+          <h1 className="title-page">EM CONSTRUÇÃO</h1>
+        </div>
+        <div className="absolute inset-y-0 right-0 w-2/5">
+          <Image
+            src={synviaImage}
+            alt="Imagem"
+            className="w-full h-full object-cover"
+            layout="responsive"
+          />
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <footer className="absolute bottom-0 right-0">
+        <div>
+          <Image
+            src={footergreenImage}
+            alt="Imagem"
+            className=""
+            layout="responsive"
+          />
+        </div>
+      </footer>
     </main>
-  )
+  );
 }
+
+/* Tailwind ======================= 
+
+
+A linha "flex flex-col items-center justify-center min-h-screen bg-white" é um exemplo de uma série de classes utilitárias do Tailwind CSS que são aplicadas a um elemento HTML. Aqui está uma explicação resumida do que cada uma dessas classes faz:
+
+flex: Essa classe transforma o elemento em um contêiner flexível, permitindo que os itens dentro dele sejam organizados em uma linha ou coluna.
+
+flex-col: Essa classe especifica que os itens dentro do contêiner devem ser organizados em uma coluna, ou seja, um item abaixo do outro.
+
+items-center: Essa classe centraliza os itens horizontalmente dentro do contêiner flexível.
+
+justify-center: Essa classe centraliza os itens verticalmente dentro do contêiner flexível.
+
+min-h-screen: Essa classe define a altura mínima do elemento como a altura total da tela (h-screen), garantindo que o elemento ocupe pelo menos toda a altura da janela do navegador.
+
+bg-white: Essa classe define o fundo do elemento como branco (white).
+No contexto específico em que essas classes são usadas, elas estão sendo aplicadas a um contêiner principal para criar um layout centralizado na tela, com os itens organizados verticalmente em uma coluna. O uso dessas classes ajuda a alinhar o conteúdo centralmente e garantir que o contêiner tenha uma altura mínima igual à altura da tela.
+
+w-full: Essa classe define a largura do elemento como 100% do contêiner pai. Ou seja, o elemento ocupará toda a largura disponível.
+
+h-full: Essa classe define a altura do elemento como 100% do contêiner pai. Isso faz com que o elemento ocupe toda a altura disponível.
+
+object-cover: Essa classe garante que a imagem seja redimensionada proporcionalmente para preencher todo o espaço disponível no elemento, mantendo sua proporção original. Isso significa que a imagem pode ser cortada ou esticada para preencher o espaço, mas sua proporção será preservada.
+
+
+*/
+
+/* ================================================================================================================= LEIAM! */
+
+// export default function Home() {
+//   return (
+//     <div style={{backgroundColor: 'red'}}>
+//       <h1 style={{ color: 'blue' }}>Minha Primeira Página</h1>
+//       <p style={{ fontSize: '16px', padding: '100px' }}>Bem-vindo ao meu aplicativo Next.js!</p>  /* Isso aqui é o CSS normal, seguindo pelo styles. */
+//     </div>
+//   );
+// }
